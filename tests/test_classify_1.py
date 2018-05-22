d7799a8d8db93693f0906ae507ea663c96b92e7c
WizenKarma <1500728@students.wits.ac.za>
2018-05-22 12:17:53 +0200

    fifth commit testing jenkins recog

--- tests/test_classify_1.py
+++ tests/test_classify_1.py
@@ -1,4 +1,16 @@
 
 import unittest
 
-from program import classify
+import classify
+
+class InequalityTest(unittest.TestCase):
+	
+	def test_equal(self):
+		a = len(classify.getData('marks.dat'))
+		b = len(classif.getData('marks.dat'))
+		self.assertEqual(a,b)
+		
+
+
+if __name__ == '__main__':
+	unittest.TestClassify()
